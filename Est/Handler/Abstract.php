b6f8889b311245a127df42a35818f36a5481ffbe
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-06-24 10:15:29 -0700

    Whitespace fix

--- Est/Handler/Abstract.php
+++ Est/Handler/Abstract.php
@@ -8,15 +8,15 @@
  */
 abstract class Est_Handler_Abstract implements Est_Handler_Interface {
 
-	/**
-	 * @var array
-	 */
-	protected $messages = array();
+    /**
+     * @var array
+     */
+    protected $messages = array();
 
-	protected $param1;
-	protected $param2;
-	protected $param3;
-	protected $value;
+    protected $param1;
+    protected $param2;
+    protected $param3;
+    protected $value;
 
     /**
      * Used by the handler to find the config value(s)
@@ -27,45 +27,45 @@ abstract class Est_Handler_Abstract implements Est_Handler_Interface {
 
 
 
-	protected $status = Est_Handler_Interface::STATUS_NOTEXECUTED;
+    protected $status = Est_Handler_Interface::STATUS_NOTEXECUTED;
 
 
-	/**
-	 * Protected method that actually applies the settings. This method is implemented in the inheriting classes and
-	 * called from ->apply
-	 *
-	 * @return bool
-	 */
-	abstract protected function _apply();
+    /**
+     * Protected method that actually applies the settings. This method is implemented in the inheriting classes and
+     * called from ->apply
+     *
+     * @return bool
+     */
+    abstract protected function _apply();
 
-	/**
-	 * Apply setting
-	 *
-	 * @return bool
-	 */
-	public function apply() {
-		try {
-			if (strtolower(trim($this->value)) == '--skip--') {
-				$this->setStatus(Est_Handler_Interface::STATUS_SKIPPED);
-				$this->addMessage(new Est_Message('Skipped because of --skip-- value'), Est_Message::SKIPPED);
-				$result = true;
-			} else {
-				$this->setStatus(Est_Handler_Interface::STATUS_RUNNING);
-				$result = $this->_apply();
-				if ($this->getStatus() == Est_Handler_Interface::STATUS_RUNNING) {
-					$this->setStatus(Est_Handler_Interface::STATUS_FINISHED);
-				}
-			}
-			return $result;
-		} catch (Exception $e) {
-			$this->setStatus(Est_Handler_Interface::STATUS_ERROR);
-			$this->addMessage(new Est_Message(
-				$e->getMessage(),
-				Est_Message::ERROR
-			));
-			return false;
-		}
-	}
+    /**
+     * Apply setting
+     *
+     * @return bool
+     */
+    public function apply() {
+        try {
+            if (strtolower(trim($this->value)) == '--skip--') {
+                $this->setStatus(Est_Handler_Interface::STATUS_SKIPPED);
+                $this->addMessage(new Est_Message('Skipped because of --skip-- value'), Est_Message::SKIPPED);
+                $result = true;
+            } else {
+                $this->setStatus(Est_Handler_Interface::STATUS_RUNNING);
+                $result = $this->_apply();
+                if ($this->getStatus() == Est_Handler_Interface::STATUS_RUNNING) {
+                    $this->setStatus(Est_Handler_Interface::STATUS_FINISHED);
+                }
+            }
+            return $result;
+        } catch (Exception $e) {
+            $this->setStatus(Est_Handler_Interface::STATUS_ERROR);
+            $this->addMessage(new Est_Message(
+                $e->getMessage(),
+                Est_Message::ERROR
+            ));
+            return false;
+        }
+    }
 
     /**
      * Extract settings
@@ -102,17 +102,17 @@ abstract class Est_Handler_Abstract implements Est_Handler_Interface {
         return $this->_register();
     }
 
-	public function setParam1($param1) {
-		$this->param1 = $param1;
-	}
+    public function setParam1($param1) {
+        $this->param1 = $param1;
+    }
 
-	public function setParam2($param2) {
-		$this->param2 = $param2;
-	}
+    public function setParam2($param2) {
+        $this->param2 = $param2;
+    }
 
-	public function setParam3($param3) {
-		$this->param3 = $param3;
-	}
+    public function setParam3($param3) {
+        $this->param3 = $param3;
+    }
 
     public function setValue($value) {
         $this->value = $value;
@@ -122,75 +122,75 @@ abstract class Est_Handler_Abstract implements Est_Handler_Interface {
         $this->configKey = $value;
     }
 
-	public function getParam1() {
-		return $this->param1;
-	}
+    public function getParam1() {
+        return $this->param1;
+    }
 
-	public function getParam2() {
-		return $this->param2;
-	}
+    public function getParam2() {
+        return $this->param2;
+    }
 
-	public function getParam3() {
-		return $this->param3;
-	}
+    public function getParam3() {
+        return $this->param3;
+    }
 
-	public function getValue() {
-		return $this->value;
-	}
+    public function getValue() {
+        return $this->value;
+    }
 
     public function getConfigKey() {
         return $this->configKey;
     }
 
-	/**
-	 * Add message
-	 *
-	 * @param $message
-	 */
-	protected function addMessage($message) {
-		$this->messages[] = $message;
-	}
+    /**
+     * Add message
+     *
+     * @param $message
+     */
+    protected function addMessage($message) {
+        $this->messages[] = $message;
+    }
 
-	/**
-	 * Get messages
-	 *
-	 * @return array
-	 */
-	public function getMessages() {
-		return $this->messages;
-	}
+    /**
+     * Get messages
+     *
+     * @return array
+     */
+    public function getMessages() {
+        return $this->messages;
+    }
 
-	/**
-	 * Get a speaking label
-	 *
-	 * @return string
-	 */
-	public function getLabel() {
-		$label = get_class($this);
-		$label .= sprintf("('%s', '%s', '%s')",
-			$this->formatParam($this->param1),
-			$this->formatParam($this->param2),
-			$this->formatParam($this->param3)
-		);
-		$label .= ' = ';
-		$label .= $this->value;
-		return $label;
-	}
+    /**
+     * Get a speaking label
+     *
+     * @return string
+     */
+    public function getLabel() {
+        $label = get_class($this);
+        $label .= sprintf("('%s', '%s', '%s')",
+            $this->formatParam($this->param1),
+            $this->formatParam($this->param2),
+            $this->formatParam($this->param3)
+        );
+        $label .= ' = ';
+        $label .= $this->value;
+        return $label;
+    }
 
-	protected function formatParam($param) {
-		if (is_null($param)) {
-			$param = 'null';
-		}
-		return $param;
-	}
+    protected function formatParam($param) {
+        if (is_null($param)) {
+            $param = 'null';
+        }
+        return $param;
+    }
 
-	protected function setStatus($status) {
-		$this->status = $status;
-	}
+    protected function setStatus($status) {
+        $this->status = $status;
+    }
 
-	public function getStatus() {
-		return $this->status;
-	}
+    public function getStatus() {
+        return $this->status;
+    }
 
 
 
--- Est/Processor.php
+++ Est/Processor.php
@@ -2,134 +2,134 @@
 
 class Est_Processor {
 
-	/**
-	 * @var string
-	 */
-	protected $environment;
+    /**
+     * @var string
+     */
+    protected $environment;
 
-	/**
-	 * @var string
-	 */
-	protected $settingsFilePath;
+    /**
+     * @var string
+     */
+    protected $settingsFilePath;
 
-	/**
-	 * @var array
-	 */
-	protected $handlerCollection;
+    /**
+     * @var array
+     */
+    protected $handlerCollection;
 
 
-	/**
-	 * Constructor
-	 *
-	 * @param $environment
-	 * @param $settingsFilePath
-	 * @throws InvalidArgumentException
-	 */
-	public function __construct($environment, $settingsFilePath) {
-		if (empty($environment)) {
-			throw new InvalidArgumentException('No environment parameter set.');
-		}
-		if (empty($settingsFilePath)) {
-			throw new InvalidArgumentException('No settings file set.');
-		}
-		if (!file_exists($settingsFilePath)) {
-			throw new InvalidArgumentException('Could not read settings file.');
-		}
+    /**
+     * Constructor
+     *
+     * @param $environment
+     * @param $settingsFilePath
+     * @throws InvalidArgumentException
+     */
+    public function __construct($environment, $settingsFilePath) {
+        if (empty($environment)) {
+            throw new InvalidArgumentException('No environment parameter set.');
+        }
+        if (empty($settingsFilePath)) {
+            throw new InvalidArgumentException('No settings file set.');
+        }
+        if (!file_exists($settingsFilePath)) {
+            throw new InvalidArgumentException('Could not read settings file.');
+        }
 
-		$this->environment = $environment;
-		$this->settingsFilePath = $settingsFilePath;
-		$this->handlerCollection = new Est_HandlerCollection();
-	}
+        $this->environment = $environment;
+        $this->settingsFilePath = $settingsFilePath;
+        $this->handlerCollection = new Est_HandlerCollection();
+    }
 
-	/**
-	 * Apply settings to current environment
-	 *
-	 * @throws Exception
-	 * @return bool
-	 */
-	public function apply() {
-		$this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
-		foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
-			$res = $handler->apply();
-			if (!$res) {
-				throw new Exception('An error in handler'.$handler->getLabel());
-			}
-		}
-		return true;
-	}
+    /**
+     * Apply settings to current environment
+     *
+     * @throws Exception
+     * @return bool
+     */
+    public function apply() {
+        $this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
+        foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
+            $res = $handler->apply();
+            if (!$res) {
+                throw new Exception('An error in handler'.$handler->getLabel());
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Apply settings to current environment
-	 *
-	 * @throws Exception
-	 * @return bool
-	 */
-	public function dryRun() {
-		$this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
-		foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
-			$this->output($handler->getLabel());
-		}
-		return true;
-	}
+    /**
+     * Apply settings to current environment
+     *
+     * @throws Exception
+     * @return bool
+     */
+    public function dryRun() {
+        $this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
+        foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
+            $this->output($handler->getLabel());
+        }
+        return true;
+    }
 
-	/**
-	 * Get value
-	 *
-	 * @param $handler
-	 * @param $param1
-	 * @param $param2
-	 * @param $param3
-	 * @throws Exception
-	 * @return Est_Handler_Abstract
-	 */
-	public function getHandler($handlerClassName, $param1, $param2, $param3) {
-		$this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
-		$handler = $this->handlerCollection->getHandler($handlerClassName, $param1, $param2, $param3);
-		if ($handler === false) {
-			throw new Exception('No handler found with given specification: '."$handlerClassName, $param1, $param2, $param3");
-		}
-		return $handler;
-	}
+    /**
+     * Get value
+     *
+     * @param $handler
+     * @param $param1
+     * @param $param2
+     * @param $param3
+     * @throws Exception
+     * @return Est_Handler_Abstract
+     */
+    public function getHandler($handlerClassName, $param1, $param2, $param3) {
+        $this->handlerCollection->buildFromSettingsCSVFile($this->settingsFilePath,$this->environment);
+        $handler = $this->handlerCollection->getHandler($handlerClassName, $param1, $param2, $param3);
+        if ($handler === false) {
+            throw new Exception('No handler found with given specification: '."$handlerClassName, $param1, $param2, $param3");
+        }
+        return $handler;
+    }
 
-	/**
-	 * Print result
-	 */
-	public function printResults() {
-		$statistics = array();
-		foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
-			// Collecting some statistics
-			$statistics[$handler->getStatus()][] = $handler;
+    /**
+     * Print result
+     */
+    public function printResults() {
+        $statistics = array();
+        foreach ($this->handlerCollection as $handler) { /* @var $handler Est_Handler_Abstract */
+            // Collecting some statistics
+            $statistics[$handler->getStatus()][] = $handler;
 
-			// skipping handlers that weren't executed
-			if ($handler->getStatus() == Est_Handler_Interface::STATUS_NOTEXECUTED) {
-				continue;
-			}
+            // skipping handlers that weren't executed
+            if ($handler->getStatus() == Est_Handler_Interface::STATUS_NOTEXECUTED) {
+                continue;
+            }
 
-			$this->output();
-			$label = $handler->getLabel();
-			$this->output($label);
-			$this->output(str_repeat('-', strlen($label)));
+            $this->output();
+            $label = $handler->getLabel();
+            $this->output($label);
+            $this->output(str_repeat('-', strlen($label)));
 
-			foreach ($handler->getMessages() as $message) { /* @var $message Est_Message */
-				$this->output($message->getColoredText());
-			}
-		}
+            foreach ($handler->getMessages() as $message) { /* @var $message Est_Message */
+                $this->output($message->getColoredText());
+            }
+        }
 
-		$this->output();
-		$this->output('Status summary:');
-		$this->output(str_repeat('=', strlen(('Status summary:'))));
+        $this->output();
+        $this->output('Status summary:');
+        $this->output(str_repeat('=', strlen(('Status summary:'))));
 
-		foreach ($statistics as $status => $handlers) {
-			$this->output(sprintf("%s: %s handler(s)", $status, count($handlers)));
-		}
+        foreach ($statistics as $status => $handlers) {
+            $this->output(sprintf("%s: %s handler(s)", $status, count($handlers)));
+        }
 
-	}
+    }
 
-	protected function output($message='', $newLine=true) {
-		echo $message;
-		if ($newLine) {
-			echo "\n";
-		}
-	}
+    protected function output($message='', $newLine=true) {
+        echo $message;
+        if ($newLine) {
+            echo "\n";
+        }
+    }
 
 }
