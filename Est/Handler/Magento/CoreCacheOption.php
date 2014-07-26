bd90b3e0ce0ed7c1f94fc7269f9a58b41f5e0440
Dmytro Zavalkin <dmytro.zavalkin@gmail.com>
2014-07-26 12:29:03 +0200

    Fixed Est_Handler_Magento_CoreCacheOption implementation, placeholders (%) aren't allowed anymore

--- Est/Handler/Magento/CoreCacheOption.php
+++ Est/Handler/Magento/CoreCacheOption.php
@@ -21,40 +21,25 @@ class Est_Handler_Magento_CoreCacheOption extends Est_Handler_Magento_AbstractDa
     {
         $this->_checkIfTableExists('core_cache_option');
 
-        $code                = $this->param1;
-        $sqlParameters       = $this->_getSqlParameters($code);
-        $containsPlaceholder = $this->_containsPlaceholder($sqlParameters);
+        $code          = trim($this->param1);
+        $sqlParameters = $this->_getSqlParameters($code);
 
         $sqlParameters[':value'] = $this->value;
-        $query                   = 'SELECT `value` FROM `' . $this->_tablePrefix . 'core_cache_option` WHERE `code` LIKE :code';
+        $query                   = 'SELECT `value` FROM `' . $this->_tablePrefix . 'core_cache_option` WHERE `code` = :code';
         $firstRow                = $this->_getFirstRow($query, $sqlParameters);
-        $action                  = self::ACTION_NO_ACTION;
 
         if ($firstRow === false) {
             $this->addMessage(
-                new Est_Message(sprintf('No rows matched code pattern "%s" found', $code), Est_Message::SKIPPED)
+                new Est_Message(sprintf('No rows with code = "%s" found', $code), Est_Message::SKIPPED)
+            );
+        } elseif ($firstRow['value'] == $this->value) {
+            $this->addMessage(
+                new Est_Message(sprintf('Value "%s" is already in place. Skipping.', $firstRow['value']), Est_Message::SKIPPED)
             );
         } else {
-            if ($containsPlaceholder) {
-                $action = self::ACTION_UPDATE;
-            } elseif ($firstRow['value'] == $this->value) {
-                $this->addMessage(
-                    new Est_Message(sprintf('Value "%s" is already in place. Skipping.', $firstRow['value']), Est_Message::SKIPPED)
-                );
-            } else {
-                $action = self::ACTION_UPDATE;
-            }
-        }
-
-        switch ($action) {
-            case self::ACTION_UPDATE:
-                $sqlParameters[':value'] = $this->value;
-                $query = 'UPDATE `' . $this->_tablePrefix . 'core_cache_option` SET `value` = :value WHERE `code` LIKE :code';
-                $this->_processUpdate($query, $sqlParameters, $firstRow['value']);
-                break;
-            case self::ACTION_NO_ACTION;
-            default:
-                break;
+            $sqlParameters[':value'] = $this->value;
+            $query = 'UPDATE `' . $this->_tablePrefix . 'core_cache_option` SET `value` = :value WHERE `code` = :code';
+            $this->_processUpdate($query, $sqlParameters, $firstRow['value']);
         }
 
         return true;
@@ -71,7 +56,7 @@ class Est_Handler_Magento_CoreCacheOption extends Est_Handler_Magento_AbstractDa
         $code = $this->param1;
         $sqlParameters = $this->_getSqlParameters($code);
 
-        $query = 'SELECT code, value FROM `' . $this->_tablePrefix . 'core_cache_option` WHERE `code` LIKE :code';
+        $query = 'SELECT code, value FROM `' . $this->_tablePrefix . 'core_cache_option` WHERE `code` = :code';
 
         return $this->_outputQuery($query, $sqlParameters);
     }
